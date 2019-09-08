a1015756a2216c980837b32192ce25aa8211683e
Lasry David <lasry.david@gmail.com>
2019-09-08 23:26:51 +0300

    design login page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,151 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="login-container">
+    <div class="left-part"></div>
+
+    <div class="right-part">
+      <div class="logo">
+        <span class="bold">חשבונית</span>
+
+        <span class="light">ירוקה</span>
+
+        <img src="../assets/svg/green_leaf.svg" class="leaf-logo" alt="leaf logo" />
+      </div>
+
+      <div class="form-container">
+        <h1>התחברות לחשבונית ירוקה</h1>
+        <form>
+          <div class="field-container">
+            <div>
+              <label class="label-container">Email</label>
+            </div>
+            <input type="text" placeholder="מייל" />
+          </div>
+
+          <div class="field-container">
+            <div class="label-container">
+              <label>password</label>
+            </div>
+            <input type="password" placeholder="סיסמה" />
+          </div>
+        </form>
+
+        <div class="buttons">
+          <button>כניסה</button>
+          <button class="google">כניסה עם גוגל</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped lang="scss">
+.login-container {
+  display: flex;
+  flex-direction: row-reverse;
+  height: 100%;
+  width: 100%;
+
+  .right-part {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem 4rem;
+
+    .logo {
+      color: #18c746;
+      font-size: 42px;
+      // text-align: right;
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: flex-end;
+
+      .light {
+        font-weight: 400;
+      }
+
+      .bold {
+        font-weight: 900;
+        margin-left: 5px;
+      }
+
+      .leaf-logo {
+        width: 25px;
+        margin-left: 5px;
+      }
+    }
+
+    .form-container {
+      h1 {
+        color: #506aed;
+      }
+
+      form {
+        input {
+          border: 0;
+          border-bottom: 1px solid black;
+          background: 0;
+          font-size: 16px;
+          padding: 5px 10px;
+          margin: 0 0 20px 0;
+        }
+      }
+
+      .buttons {
+        margin: 2rem 0 0 0;
+
+        button {
+          font-size: 1rem;
+          border-radius: 5rem;
+          background: #506aed;
+          border: 1px solid #506aed;
+          margin: 0 0.5rem;
+          color: #ffffff;
+          cursor: pointer;
+          display: inline-block;
+          padding: 0 2rem;
+          line-height: 2.5rem;
+          text-decoration: none;
+          white-space: nowrap;
+          text-align: center;
+          -webkit-transition: width 0.25s ease-in-out;
+          -moz-transition: width 0.25s ease-in-out;
+          transition: width 0.25s ease-in-out;
+          outline: none;
+          -webkit-touch-callout: none;
+          -webkit-user-select: none;
+          -khtml-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          -webkit-tap-highlight-color: transparent;
+
+          &.google {
+            background: #ffffff;
+            color: #506aed;
+          }
+        }
+      }
+    }
+  }
+
+  .left-part {
+    background: url(../assets/svg/green_login_page.svg);
+    height: 100%;
+    background-repeat: no-repeat;
+    background-color: #ffdcdc;
+    background-position: right center;
+    background-size: 80%;
+    flex: 1;
+  }
 }
 </style>
